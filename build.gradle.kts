4fb0f31f5f1ffe43746e017f9f87152e4a33439e
MichaelWY <Michael@KronosAD.com>
2024-05-30 12:08:26 -0700

    added 26 topics to topiclist.txt, added /say command, added /setstatus command (to manually enter activities rather than pull from the list if desired)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.michaelthesad.sadbot"
-version = "1.1"
+version = "1.2"
 
 repositories {
     mavenCentral()
--- src/main/java/com/michaelthesad/sadbot/commands/CommandManager.java
+++ src/main/java/com/michaelthesad/sadbot/commands/CommandManager.java
@@ -6,11 +6,15 @@ import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static com.michaelthesad.sadbot.sadbot.shardManager;
@@ -25,11 +29,10 @@ public class CommandManager extends ListenerAdapter {
         }
         if (command.equals("newtopic")) {
             //command /newtopic
-            //the event.reply here will go away eventually ;)
             String randomTopic = sadbot.getTopicList();
             event.reply(randomTopic).queue();
             System.out.println(randomTopic);
-            java.util.Date date= new java.util.Date();
+            Date date = new Date();
             System.out.println("^" + new Timestamp(date.getTime()));
         }
         if (command.equals("newstatus")) {
@@ -38,27 +41,49 @@ public class CommandManager extends ListenerAdapter {
             System.out.println("Status changed to:");
             System.out.println(randomStatus);
             shardManager.setActivity(Activity.customStatus(randomStatus));
-            event.reply("The activity has been updated!").setEphemeral(true).queue();
+            event.reply("The activity has been updated via random selection!").setEphemeral(true).queue();
+        }
+        else if (command.equals("say")) {
+            //command /say <message>
+            OptionMapping messageOption = event.getOption("message");
+            assert messageOption != null;
+            String message = messageOption.getAsString();
+            event.getChannel().sendMessage(message).queue();
+            event.reply("Message sent!").setEphemeral(true).queue();
+        }
+        if (command.equals("setstatus")) {
+            //command /setstatus <message>
+            OptionMapping messageOption = event.getOption("message");
+            assert messageOption != null;
+            String message = messageOption.getAsString();
+            shardManager.setActivity(Activity.customStatus(message));
+            event.reply("The activity has been updated via your request!").setEphemeral(true).queue();
         }
     }
 
     // Guild command
-
     @Override
     public void onGuildReady(GuildReadyEvent event) {
         List<CommandData> commandData = new ArrayList<>();
-        commandData.add(Commands.slash("introduce", "sadbot will introduce itself"));
-        commandData.add(Commands.slash("newtopic", "forces the bot to pull a new topic"));
-        commandData.add(Commands.slash("newstatus", "sadbot will have a new status if you use this"));
+        commandData.add(Commands.slash("introduce", "sadbot will introduce himself"));
+        commandData.add(Commands.slash("newtopic", "sadbot will pull a new topic"));
+        commandData.add(Commands.slash("newstatus", "sadbot will select a new status via the random list"));
+        OptionData option1 = new OptionData(OptionType.STRING, "message", "The message you want the bot to say", true);
+        commandData.add(Commands.slash("say", "sadbot will repeat the entered text").addOptions(option1));
+        commandData.add(Commands.slash("setstatus", "sadbot will have a new status").addOptions(option1));
         event.getGuild().updateCommands().addCommands(commandData).queue();
     }
 
     @Override
     public void onGuildJoin(GuildJoinEvent event) {
         List<CommandData> commandData = new ArrayList<>();
-        commandData.add(Commands.slash("introduce", "sadbot will introduce itself"));
-        commandData.add(Commands.slash("newtopic", "forces the bot to pull a new topic"));
-        commandData.add(Commands.slash("newstatus", "sadbot will have a new status if you use this"));
+        commandData.add(Commands.slash("introduce", "sadbot will introduce himself"));
+        commandData.add(Commands.slash("newtopic", "sadbot will pull a new topic"));
+        commandData.add(Commands.slash("newstatus", "sadbot will select a new status via the random list"));
+        OptionData option1 = new OptionData(OptionType.STRING, "message", "The message you want the bot to say", true);
+        commandData.add(Commands.slash("say", "sadbot will repeat the entered text").addOptions(option1));
+        commandData.add(Commands.slash("setstatus", "sadbot will have a new status").addOptions(option1));
+
         event.getGuild().updateCommands().addCommands(commandData).queue();
     }
 }
